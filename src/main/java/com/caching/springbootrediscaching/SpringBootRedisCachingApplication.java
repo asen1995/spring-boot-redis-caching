3617e58a9c108879eed0318f3a810dd3eeeecb9b
Asen Nikolaev <asen.nikolaev95@abv.bg>
2023-05-21 14:14:33 +0300

    init empty project

---
+++ src/main/java/com/caching/springbootrediscaching/SpringBootRedisCachingApplication.java
@@ -0,0 +1,13 @@
+package com.caching.springbootrediscaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisCachingApplication.class, args);
+    }
+
+}
